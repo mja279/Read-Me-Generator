0f5545bf7652702182c6a1598c19c3e4f053745c
Maria Jimena <mja279@georgetown.edu>
2020-08-07 15:58:18 -0400

    pushing updates

--- script.js
+++ script.js
@@ -1,6 +1,50 @@
-// array of questions for user
+ // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?"
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+      },
+      {
+        type: "input",
+        name: "project",
+        message: "What is your project's name?"
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Please write a short description of your project:"
+      },
+      {
+        type: "input",
+        name: "license",
+        message: "What type of license should your project have?"
+      },
+      {
+        type: "input",
+        name: "dependencies",
+        message: "What command should be run to install dependencies?"
+      },
+      {
+        type: "input",
+        name: "tests",
+        message: "What command should be run to run tests?"
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "What does the user need to know about using the repo?"
+      },
+      {
+        type: "input",
+        name: "contribution",
+        message: "What does the user need to know about contributing to the repo?"
+      },
 ];
 
 // function to write README file
